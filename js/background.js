8796b6b15c9f5021d163304cf4d441a7bcf57489
anzeen <anjh3407@naver.com>
2022-09-20 21:41:11 +0900

    first commit

---
+++ js/background.js
@@ -0,0 +1,13 @@
+const images = [
+    "0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+// append - 맨 아래
+// prepend - 맨 위
