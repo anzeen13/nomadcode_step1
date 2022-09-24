5a4f1c1e9c3e8c81b9363c5f6f0bdf8772f8486e
anzeen <anjh3407@naver.com>
2022-09-24 18:40:52 +0900

    first commit

--- js/background.js
+++ js/background.js
@@ -5,7 +5,7 @@ const images = [
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
-
+bgImage.classList.add("bgImage");
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
