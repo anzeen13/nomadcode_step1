e93dc5f446e2774801d2bfa217a7be8960e9f973
anzeen <anjh3407@naver.com>
2022-10-03 15:21:21 +0900

    daily update

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 const images = [
-    "0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg"
+    "0.jpeg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
